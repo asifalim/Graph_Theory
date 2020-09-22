ea66e9e03ca71b2df778692068680cb0aa4c25c1
Alim Uddin Asif <66546728+asifalim@users.noreply.github.com>
2020-09-23 01:22:30 +0600

    Update minimum_spanning_tree.cpp

--- minimum_spanning_tree.cpp
+++ minimum_spanning_tree.cpp
@@ -1,15 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-struct edge
-{
-    int a;int b;int c;
-};
-edge ar[100002];
-int parent[1003];
-bool cmp(edge a,edge b)
-{
-    return a.c<b.c;
-}
+vector<int>parent;
 int find(int a)
 {
     if(parent[a]==-1)return a;
@@ -23,15 +14,17 @@ int main()
 {
     int n,m,i,sm=0,c,d,k;
     cin>>n>>m;
-    for(i=0;i<m;i++)cin>>ar[i].a>>ar[i].b>>ar[i].c;
-    for(i=1;i<=n;i++)parent[i]=-1;
-    sort(ar,ar+m,cmp);
+    array<ll,3>ar[m];
+    for(i=0;i<m;i++)cin>>ar[i][1]>>ar[i][2]>>ar[i][0];
+    sort(ar,ar+m);parent.assign(n+1,-1);
     for(i=0;i<m;i++)
     {
-        c=find(ar[i].a);
-        d=find(ar[i].b);
-        if(c!=d)sm+=ar[i].c,merge(c,d);
+        a=find(ar[i][1]);
+        b=find(ar[i][2]);
+        if(a!=b)sm+=ar[i][0],merge(a,b),cn++;
     }
-
+    if(cn<n-1)cout<<"IMPOSSIBLE"<<endl;
+    else
     cout<<sm<<endl;
+    return 0;
 }
