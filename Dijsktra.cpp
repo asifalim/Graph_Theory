26cebf0e52500f4831b079652b10c1c0c2856582
Alim Uddin Asif <66546728+asifalim@users.noreply.github.com>
2020-12-03 20:39:44 +0600

    Update Dijsktra.cpp

--- Dijsktra.cpp
+++ Dijsktra.cpp
@@ -1,52 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define ll long long int
-vector<pair<ll,ll>>adj[10001];
-vector<ll>dis,vis;
-void ans(ll n)
+#define endl "\n"
+#define INF 1e18
+const int mxN=1e5+3;
+vector<pair<int,int>>adj[mxN];
+vector<ll>dis,vis,par,v;
+void dij(int n)
 {
-    priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>>pq;
+    priority_queue<pair<ll,int>,vector<pair<ll,int>>,greater<pair<ll,int>>>pq;
     pq.push({0,n});
     dis[n]=0;
+    par[n]=0;
     while(!pq.empty())
     {
-        int node=pq.top().second;
-        int node_dis=pq.top().first;
+        ll node=pq.top().second,node_dis=pq.top().first;
         pq.pop();
-        if(vis[node])continue;vis[node]=1;  //make faster
-        for(pair<ll,ll>edge:adj[node])
+        if(vis[node])continue;
+        vis[node]=1;
+        for(pair<int,int>edge:adj[node])
         {
-            if(node_dis+edge.second<dis[edge.first])
-            {
-                dis[edge.first]=node_dis+edge.second;
-                pq.push({dis[edge.first],edge.first});
-            }
+            if(node_dis+edge.second<dis[edge.first])dis[edge.first]=node_dis+edge.second,pq.push({dis[edge.first],edge.first}),par[edge.first]=node;
         }
     }
 }
 int main()
 {
-    ll k;
-    cin>>k;
-    while(k--)
+    int n,a,b,c,m,i,j,cn=0,sm=0,mx=INT_MIN,mn=INT_MAX,k;
+    string s,st,st1;
+    cin>>n>>m;
+    dis.assign(n+1,INF);
+    vis.assign(n+1,0),par.assign(n+1,0);
+    for(i=0; i<m; i++)cin>>a>>b>>c,adj[a].push_back({b,c}),adj[b].push_back({a,c});
+    dij(1);
+    v.push_back(n);
+    if(dis[n]==INF)
     {
-        ll t,n,a,b,w,i,j,c,d;
-        cin>>t>>n;
-        dis.assign(t+1,1999999999);
-        vis.assign(t+1,0);
-        memset(adj,0,sizeof(adj));
-        for(i=0; i<n; i++)
-        {
-            cin>>a>>b>>w;
-            adj[a].push_back({b,w});
-        }
-        cin>>c>>d;
-        ans(c);
-        if(dis[d]==1999999999)
-        {
-            cout<<"NO"<<endl;
-            continue;
-        }
-        cout<<dis[d]<<endl;
+        cout<<-1<<endl;
+        return 0;
     }
+    while(par[n])v.push_back(par[n]),n=par[n];
+    for(i=v.size()-1; i>=0; i--)cout<<v[i]<<" ";
+    cout<<endl;
+    return 0;
 }
+
