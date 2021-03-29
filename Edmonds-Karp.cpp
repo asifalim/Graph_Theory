5251a7e0b2525495182ed0d2e4d9812f557f01cb
Alim Uddin Asif <66546728+asifalim@users.noreply.github.com>
2021-03-30 00:41:46 +0600

    Update Edmonds-Karp.cpp

--- Edmonds-Karp.cpp
+++ Edmonds-Karp.cpp
@@ -1,64 +1,68 @@
 #include<bits/stdc++.h>
-#define ll long long int
 using namespace std;
-const int mxN=1e4+2;
-ll INF=1e61;              //O(V*E^2)
-vector<ll>adj[mxN];
-ll cc[mxN][mxN];
-ll bfs(ll s,ll t,vector<ll>&parent)
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define dbg(a,b,c) cout<<a<<"  "<<b<<"  "<<c<<endl;
+#define mem(a,b) memset(a,b,sizeof(a))
+#define endl "\n"
+#define INF 1e9
+#define w(t) cin>>t;while(t--)
+#define kill(a) {cout<<a<<endl;continue;}
+#define pi  2 * acos(0.0)
+int t,ans=0,tot=0,k=0;
+const int mxN=515,mod=1e9+7;
+vector<int>adj[mxN];                    //O(V*E^2)
+ll ar[mxN][mxN];int par[mxN];
+int bfs(int s,int t)
 {
-    fill(parent.begin(),parent.end(),-1);
-    parent[s]=-2;
-    queue<pair<ll,ll>>q;
+    mem(par,-1);
+    queue<pair<int,int>>q;
     q.push({s,INF});
     while(!q.empty())
     {
-        ll cur=q.front().first,flow=q.front().second;
-        q.pop();
-        for(auto next:adj[cur])
+        int node=q.front().first,flow=q.front().second;q.pop();
+        for(auto i:adj[node])
         {
-            if(parent[next]==-1 and cc[cur][next])
+            if(ar[node][i] and par[i]==-1)
             {
-                parent[next]=cur;
-                ll new_flow=min(flow,cc[cur][next]);
-                if(next==t)return new_flow;
-                q.push({next,new_flow});
+                flow=min(1LL*flow,ar[node][i]);
+                par[i]=node;
+                if(i==t)return flow;
+                q.push({i,flow});
             }
         }
     }
     return 0;
 }
-ll max_flow(ll s,ll t)
+ll Edmonds_Karp(int s,int t)
 {
-    vector<ll>parent(mxN);
-    ll flow=0,new_flow;
-    while(new_flow=bfs(s,t,parent))
+    int prev,new_flow,cur;ll flow=0;
+    while(new_flow=bfs(s,t))
     {
+        cur=t;
         flow+=new_flow;
-        ll cur=t;
         while(cur!=s)
         {
-            ll prev=parent[cur];
-            cc[prev][cur]-=new_flow;
-            //cc[cur][prev]+=new_flow;
+            prev=par[cur];
+            ar[prev][cur]-=new_flow;
+            ar[cur][prev]+=new_flow;
             cur=prev;
         }
     }
     return flow;
 }
-int main()
+signed main()
 {
-    ll n,m,i,j,a,b,c;
-    cin>>n>>m;
-    for(i=0; i<m; i++)
-    {
-        cin>>a>>b>>c;
-        adj[a].push_back(b);
-        //adj[b].push_back(a);
-        cc[a][b]+=c;
-        //cc[b][a]+=c;     /*For undirected
-    }
-    cout<<max_flow(1,n)<<endl;    //Source 1 and sink n
+    //fast;
+   // w(t)
+    //{
+        int n,m,a,b,c,d,i,j,sm=0,sm1=0,cn=0,cn1=0,mx=INT_MIN,mn=INT_MAX,mx1;
+        string s,ss,sr;
+        bool f=false,ff=false;
+        cin>>n>>m;
+        for(i=1;i<=m;i++)cin>>a>>b>>c,adj[a].push_back(b),adj[b].push_back(a),ar[a][b]+=c;   //for undirected  ---> ar[b][a]+=c;
+        cout<<Edmonds_Karp(1,n)<<endl;
+    //}
     return 0;
 }
 
