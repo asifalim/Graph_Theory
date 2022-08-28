ecbb632758d968d824ad4c8dcc122dc5c2f3afa4
Alim Uddin Asif <66546728+asifalim@users.noreply.github.com>
2022-08-28 19:55:19 +0600

    Update Floyd Warshall.cpp

--- Floyd Warshall.cpp
+++ Floyd Warshall.cpp
@@ -4,31 +4,29 @@
 using namespace std;
 int main()
 {
-    ll n,m,i,j,t,k,a,b,c,d,e;
-    cin>>n>>m>>t;
-    vector<vector<ll>>adj(n+1,vector<ll>(n+1,INF));
-    for(i=1; i<=n; i++)adj[i][i]=0;
-    while(m--)
+    ll n,m,a,b,c;
+    cin>>n>>m;
+    ll adj[n+1][n+1];
+    for(int i=1;i<=n;i++)for(int j=1;j<=n;j++)if(i==j)adj[i][i]=0;else adj[i][j]=INF;
+    for(int i=1; i<=m; i++)
     {
         cin>>a>>b>>c;
         adj[a][b]=min(c,adj[a][b]);
         adj[b][a]=min(c,adj[b][a]);
     }
-    for(k=1; k<=n; k++)
+    for(int k=1; k<=n; k++)
     {
-        for(i=1; i<=n; i++)
+        for(int i=1; i<=n; i++)
         {
-            for(j=1; j<=n; j++)
+            for(int j=1; j<=n; j++)
             {
-                if(adj[i][k]<INF and adj[k][j]<INF)adj[i][j]=min(adj[i][j],adj[i][k]+adj[k][j]);
+                adj[i][j]=min(adj[i][j],adj[i][k]+adj[k][j]);
             }
         }
     }
-    while(t--)
+    /*for(int i=1;i<=n;i++)
     {
-        cin>>d>>e;
-        if(adj[d][e]==INF)cout<<-1<<'\n';
-        else cout<<adj[d][e]<<'\n';
-    }
+        for(int j=1;j<=n;j++)cout<<adj[i][j]<<" ";cout<<endl;
+    }*/
     return 0;
 }
