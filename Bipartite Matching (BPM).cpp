9fc079d17f225df694189411b7f00d41167858fa
Alim Uddin Asif <66546728+asifalim@users.noreply.github.com>
2024-01-13 23:26:34 +0600

    updated

--- Bipartite Matching (BPM).cpp
+++ Bipartite Matching (BPM).cpp
@@ -1,52 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define ll long long int
-#define dbg(a,b,c) cerr<<a<<"  "<<b<<"  "<<c<<endl;
-#define mem(a,b) memset(a,b,sizeof(a))
-#define endl "\n"
-#define INF 1e12
-#define w(t) cin>>t;while(t--)
+#define dbg(a,b,c,d) cerr<<a<<"  "<<b<<"  "<<c<<"  "<<d<<endl;
 #define kill(a) {cout<<a<<endl;continue;}
+#define KILL(a) {cout<<a<<endl;return 0;}
+#define debug cerr<<"Error Found"<<endl;
+#define mem(a,b) memset(a,b,sizeof(a))
+#define lcm(a, b) (a/__gcd(a,b))*b
+#define w(t) cin>>t;while(t--)
 #define pi  2 * acos(0.0)
-int t,ans=0,tot=0,kk=0;
-const int mxn=1e3+3,mod=1e9+7;
+#define ll long long int
+#define endl "\n"
+#define INF (1LL<<61)
+int t, cs = 0;
+const int mxn = 1e5 + 5, mod = 1e9 + 7;
+bool vis[mxn], color[mxn];
 vector<int>adj[mxn];
-int match[mxn];
-bool vis[mxn];
-bool dfs(int n)
+bool bfs(int root)
 {
-    for(auto i:adj[n])
+    queue<int>q;
+    q.push(root);
+    while(!q.empty())
     {
-        if(vis[i])continue;
-        vis[i]=true;
-        if(match[i]==-1 or dfs(match[i]))
+        int node = q.front();
+        q.pop();
+        if(vis[node])continue;
+        vis[node] = true;
+        for(auto i:adj[node])
         {
-            match[i]=n;
-            match[n]=i;
-            return 1;
+           if(vis[i])
+           {
+               if(color[i] == color[node])return false;
+               continue;
+           }
+           color[i] = (!color[node]);
+           q.push(i);
         }
     }
-    return 0;
+    return true;
 }
-signed main()
+int32_t main()
 {
-
-    //fast;
-    w(t)
-    {
-        int n,m,a,b,c,d,e,i,j,k,sm=0,sm1=0,cn=0,cn1=0,mx=-1e9,mn=1e9;
-        string s,ss,sr,sa;
-        bool f=false,ff=false;
-        cin>>n>>m;mem(match,-1);
-        for(i=0; i<m; i++)cin>>a>>b,adj[a].push_back(b),adj[b].push_back(a);
-        for(i=1; i<=n; i++)
-        {
-            mem(vis,false);
-            if(~match[i] or !dfs(i))cn++;
-        }
-        for(i=1; i<=n; i++)adj[i].clear();
-
-        cout<<"Case "<<++tot<<": "<<cn<<endl;
-    }
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0, a, b; i < m; i++)cin >> a >> b, adj[a].push_back(b), adj[b].push_back(a);
+    bool f = true;
+    for(int i = 1; i <= n and f; i++)if(!vis[i])if(!bfs(i))f = false;
+    if(!f)KILL("IMPOSSIBLE");
+    for(int i = 1; i <= n; i++)cout << color[i] + 1 <<" ";cout << endl;
 }
