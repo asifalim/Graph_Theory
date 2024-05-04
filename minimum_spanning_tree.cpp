3487cb3e32221e9d8d33e5e3071c50145dea8410
Alim Uddin Asif <66546728+asifalim@users.noreply.github.com>
2024-05-04 21:14:12 +0600

    Update minimum_spanning_tree.cpp
    done spacing

--- minimum_spanning_tree.cpp
+++ minimum_spanning_tree.cpp
@@ -1,30 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int>parent;
-int find(int a)
+const int mxn = 2e6 + 3;
+int par[mxn];
+void refresh(int n)
 {
-    if(parent[a]==-1)return a;
-    return parent[a]=find(parent[a]);
+    for(int i = 1; i <= n; i++)par[i] = i;
 }
-void merge(int a,int b)
+int find(int n)
 {
-    parent[a]=b;
+    return par[n] == n ? n: par[n] = find(par[n]);
 }
 int main()
 {
-    int n,m,i,sm=0,c,d,k;
+    int n, m;
     cin>>n>>m;
-    array<ll,3>ar[m];
-    for(i=0;i<m;i++)cin>>ar[i][1]>>ar[i][2]>>ar[i][0];
-    sort(ar,ar+m);parent.assign(n+1,-1);
-    for(i=0;i<m;i++)
+    refresh(n);
+    array<int, 3>ar[m];
+    for(int i = 0; i < m; i++)cin>> ar[i][1] >> ar[i][2] >> ar[i][0];
+    sort(ar, ar + m);
+    int64_t cost = 0;
+    int cmp = 0;
+    for(int i = 0; i < m; i++)
     {
-        a=find(ar[i][1]);
-        b=find(ar[i][2]);
-        if(a!=b)sm+=ar[i][0],merge(a,b),cn++;
+        int a = find(ar[i][1]);
+        int b = find(ar[i][2]);
+        if(a != b)cost += ar[i][0], merge(a,b), cmp++;
     }
-    if(cn<n-1)cout<<"IMPOSSIBLE"<<endl;
-    else
-    cout<<sm<<endl;
+    if(cn < n - 1)cout << "IMPOSSIBLE" << endl;
+    else cout << cost << endl;
     return 0;
 }
